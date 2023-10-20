a2d988f0c67e7efc755651a7a90f0640ef86537f
denysastapov <purple.rain1982@gmail.com>
2023-10-20 19:26:06 +0300

    Initial Commit

---
+++ test_controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_controllers
+//
+//  Created by Denys Astapov on 20.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
