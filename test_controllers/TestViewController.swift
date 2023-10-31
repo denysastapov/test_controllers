62bc24201d909cbaebc4b70c9628a79fb61df4c7
denysastapov <purple.rain1982@gmail.com>
2023-10-31 16:50:09 +0200

    Changed files

--- test_controllers/TestViewController.swift
+++ test_controllers/TestViewController.swift
@@ -11,19 +11,38 @@ class TestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .cyan
+        self.title = "Test View Controller"
+//        if navigationController?.viewControllers.isEmpty == false {
+//            let back_btn = UIButton(frame: CGRect(x: view.bounds.width / 2 - 75, y: view.bounds.height / 3, width: 150, height: 50))
+//            back_btn.setTitle("Back", for: .normal)
+//            back_btn.setTitleColor(.black, for: .normal)
+//            back_btn.layer.cornerRadius = 10
+//            back_btn.backgroundColor = .systemYellow
+//            back_btn.addAction(.init(handler: { _ in
+//                self.navigationController?.popViewController(animated: true)
+//            }), for: .touchUpInside)
+//            self.view.addSubview(back_btn)
+//        }
+        if parent != nil || navigationController?.viewControllers.isEmpty == false {
+            let goBackBtn = UIButton()
+            goBackBtn.setTitle("Go back 🔙", for: .normal)
+            goBackBtn.setTitleColor(.black, for: .normal)
+            goBackBtn.layer.cornerRadius = 10
+            goBackBtn.backgroundColor = .systemYellow
+            goBackBtn.addAction(.init(handler: { _ in
+                self.dismiss(animated: true)
+                self.navigationController?.popViewController(animated: true)
+            }), for: .touchUpInside)
+            goBackBtn.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(goBackBtn)
+            
+            let goBackBtnHorizontalConstraint = NSLayoutConstraint(item: goBackBtn, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
+            let goBackBtnVerticalConstraint = NSLayoutConstraint(item: goBackBtn, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 400)
+            let goBackBtnWidthConstraint = NSLayoutConstraint(item: goBackBtn, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 150)
+            let goBackBtnHeightConstraint = NSLayoutConstraint(item: goBackBtn, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 50)
+            let goBackBtnConstraints = [goBackBtnHorizontalConstraint, goBackBtnVerticalConstraint, goBackBtnWidthConstraint, goBackBtnHeightConstraint]
+            NSLayoutConstraint.activate(goBackBtnConstraints)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- test_controllers/ViewController.swift
+++ test_controllers/ViewController.swift
@@ -7,13 +7,131 @@
 
 import UIKit
 
+enum NavigationType {
+    case present, push
+}
+
 class ViewController: UIViewController {
+
+    class BeautyButton: UIButton {
+        
+        init(title: String, titleColor: UIColor, backgroundColor: UIColor) {
+            super.init(frame: .zero)
+            self.setTitle(title, for: .normal)
+            self.setTitleColor(titleColor, for: .normal)
+            self.backgroundColor = backgroundColor
+            self.layer.cornerRadius = 10
+            self.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                self.widthAnchor.constraint(equalToConstant: 150),
+                self.heightAnchor.constraint(equalToConstant: 50)
+            ])
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+    
+    init(navigationType: NavigationType? = nil){
+        self.navigationType = navigationType
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private let navigationType: NavigationType?
+    private let newPresentBtn = BeautyButton(title: "New Present", titleColor: .white, backgroundColor: .black)
+    private let presentBtn = BeautyButton(title: "Present", titleColor: .white, backgroundColor: .systemPink)
+    private let pushBtn = BeautyButton(title: "Push", titleColor: .white, backgroundColor: .systemBlue)
+    private let goBackBtn = BeautyButton(title: "Go back 🔙", titleColor: .black, backgroundColor: .systemYellow)
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "Main View Controller"
+        self.view.backgroundColor = .systemBackground
+        
+        setUpStackView()
+        
+        setUpPresentBtn()
+        setUpPushBtn()
+        setUpGoBackBtn()
+        
+        goBackBtn.isHidden = navigationType == nil
     }
+    
+    private func setUpPresentBtn() {
+        presentBtn.addAction(.init(handler: { [unowned self] _ in
+            let navModalViewController = UINavigationController(rootViewController: ViewController(navigationType: .present))
+            present(navModalViewController, animated: true)
+        }), for: .touchUpInside)
+        self.view.addSubview(presentBtn)
+        
+        NSLayoutConstraint.activate([
+            presentBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            presentBtn.topAnchor.constraint(equalTo: view.topAnchor, constant: 300)
+        ])
+    }
+    
+    private func setUpPushBtn() {
+        pushBtn.addAction(.init(handler: { [unowned self] _ in
+            let testViewController = ViewController(navigationType: .push)
+            self.navigationController?.pushViewController(testViewController, animated: true)
+        }), for: .touchUpInside)
+        self.view.addSubview(pushBtn)
+        
+        NSLayoutConstraint.activate([
+            pushBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pushBtn.topAnchor.constraint(equalTo: presentBtn.bottomAnchor, constant: 50)
+        ])
+    }
+    
+    private func setUpGoBackBtn() {
+        goBackBtn.isHidden = true
+        goBackBtn.addAction(.init(handler: { [unowned self] _ in
+            switch navigationType {
+            case .present:
+                dismiss(animated: true)
+            case .push:
+                navigationController?.popViewController(animated: true)
+            case nil:
+                return
+            }
+        }), for: .touchUpInside)
+        self.view.addSubview(goBackBtn)
+        
+        NSLayoutConstraint.activate([
+            goBackBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            goBackBtn.topAnchor.constraint(equalTo: pushBtn.bottomAnchor, constant: 50)
+        ])
+    }
+    
+    private func setUpStackView() {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 50
+        stackView.distribution = .equalCentering
+        stackView.alignment = .center
+        view.addSubview(stackView)
+        
+        stackView.addArrangedSubview(presentBtn)
+        stackView.addArrangedSubview(pushBtn)
+        stackView.addArrangedSubview(goBackBtn)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
 
-
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor)
+        ])
+    }
+    
 }
+
 
